0a94122e7b397aeff408262eeebf5d751d753041
TyAssis <tyassis3@gmail.com>
2019-07-21 12:44:30 -0300

    Add buttons to modal

--- src/components/Burguer/OrderSummary/OrderSummary.js
+++ src/components/Burguer/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
 
@@ -20,6 +21,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p> Continue to checkout? </p>
+            <Button buttonType={"Danger"} clicked={props.purchaseCanceled}> CANCEL </Button>
+            <Button buttonType={"Success"} clicked={props.purchaseContinue}> CONTINUE </Button>
         </Aux>
     )
 }
